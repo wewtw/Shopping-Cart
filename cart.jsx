2301e7e41556defaa2a842e5ea0c6b4477653d71
Edar <108901257+wewtw@users.noreply.github.com>
2022-12-14 16:46:46 -0500

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -129,7 +129,7 @@ const Products = (props) => {
         <Button variant="primary" size="large">
           {item.name} Cost:${item.cost} Stock:{item.instock}
         </Button>
-        <input name={item.name} type="submit" onClick={addToCart}></input>
+        <input name={item.name} type="submit" onClick={addToCart} value='Add to Cart'></input>
       </li>
     );
   });
